6d2f909a4a5251bd0e2772aeff3387258009c633
root <root@rmparts.eu>
2024-09-12 12:58:37 +0000

    Add .gitignore and remove large files

--- pub/media/mageplaza/search/default_2_addition.js
+++
@@ -1,3 +0,0 @@
-;var mp_new_product_search = [];
-            var mp_most_viewed_products = [];
-            var mp_bestsellers = [{"value":"valve","s":"057127247A","c":["38","1","2","44","1","2","38"],"d":"","p":45.6,"i":"placeholder\/default\/PHOTO_COMMING_SOON_9.png","u":"057127247A-valve-bvc0B0.html","o":"bestsellers"}];
