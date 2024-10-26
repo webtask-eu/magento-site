d427e22ca414ca118c2a789ab1494f2365aa8a1c
Yuriy <webtask.eu@gmail.com>
2024-10-26 16:50:26 +0300

    module

---
+++ app/code/Vendor/CategoryFormImageUploaderFix/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Vendor_CategoryFormImageUploaderFix',
+    __DIR__
+);
+
